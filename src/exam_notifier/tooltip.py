8f6133e6ff679180bbe60ad72fa7132d87d151d1
Glutanimate <glutanimate@users.noreply.github.com>
2020-02-24 13:09:46 +0100

    We're already in the future

--- src/exam_notifier/tooltip.py
+++ src/exam_notifier/tooltip.py
@@ -33,9 +33,6 @@
 Module template.
 """
 
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-
 from anki.hooks import addHook
 from aqt import mw
 from aqt.utils import tooltip
