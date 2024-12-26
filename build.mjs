5bf97f5d8c7abf04d156073a79bcb6ee1c25d34b
Abdo <abdo@abdnh.net>
2024-12-26 21:04:20 +0300

    Update for Anki 24.11

---
+++ build.mjs
@@ -0,0 +1,51 @@
+import * as fs from "fs";
+import { env } from "process";
+
+import * as esbuild from "esbuild";
+import { sveltePreprocess } from "svelte-preprocess";
+import sveltePlugin from "esbuild-svelte";
+
+for (const dir of ["../dist", "../dist/web"]) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+}
+
+
+const production = env.NODE_ENV === "production";
+const watch = env.WATCH === "true";
+
+
+const entryPoints = ["ts/deck_options/index.js"];
+
+const options = {
+    entryPoints,
+    outfile: "./src/exam_notifier/web/deck_options.js",
+    format: "iife",
+    target: ["es6", "chrome77"],
+    bundle: true,
+    minify: production,
+    treeShaking: production,
+    sourcemap: !production,
+    pure: production ? ["console.log", "console.time", "console.timeEnd"] : [],
+    // external: ["svelte"],
+    plugins: [
+      sveltePlugin({
+        compilerOptions: { css: 'injected'},
+        preprocess: sveltePreprocess(),
+        // include: /\.(?:svelte)|(?:svg)$/,
+      }),
+    ],
+    loader: { ".svg": "text" },
+};
+
+const context = await esbuild.context(options);
+
+if (watch) {
+    console.log("Watching for changes...");
+    await context.watch();
+}
+else {
+    await context.rebuild();
+    context.dispose();
+}
