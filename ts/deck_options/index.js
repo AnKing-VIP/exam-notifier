00bdd9bf31557dec09985bcc26ecfc8e1ba0be41
Abdo <abdo@abdnh.net>
2024-12-26 21:04:20 +0300

    WIP: update for Anki 24.11

--- ts/deck_options/index.js
+++ ts/deck_options/index.js
@@ -1,5 +1,21 @@
+import { mount } from "svelte";
+import { get } from "svelte/store";
+
 import DeckOptionsAddon from "./DeckOptionsAddon.svelte";
 
 $deckOptions.then((deckOptions) => {
-  deckOptions.addSvelteAddon({ component: DeckOptionsAddon });
+    deckOptions.addHtmlAddon("", () => {});
+    setTimeout(() => {
+        let target = null;
+        for (const h1 of document.querySelectorAll("h1")) {
+            if (h1.textContent === "Add-ons") {
+                target = h1.parentElement;
+                break;
+            }
+        }
+        mount(DeckOptionsAddon, {
+            target: target,
+            props: { data: get(deckOptions.auxData()) },
+        });
+    }, 100);
 });
